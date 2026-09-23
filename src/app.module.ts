9cc61c3bd0ba27a0331412204841f7759fdf5fe7
Vinicius Alves <vinicius.alves@sou.inteli.edu.br>
2026-09-23 14:42:20 -0300

    feat(prisma): adiciona PrismaService/PrismaModule reutilizável

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import Joi from 'joi';
 import { HealthModule } from './health/health.module';
+import { PrismaModule } from './prisma/prisma.module';
 @Module({
   imports: [
+    PrismaModule,
     ConfigModule.forRoot({
       isGlobal: true,
       ignoreEnvFile: process.env.NODE_ENV === 'test',
